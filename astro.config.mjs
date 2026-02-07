f5506bdb315b49697e90f91c1c2da48c50ea3921
Diego Tenjo <tenjodiego961@gmail.com>
2026-02-06 20:30:14 -0500

    feat(ui): Implement dark mode toggle and dynamic greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-tutorial.tenjodiego961.workers.dev/",
   output: "server",
+
   adapter: cloudflare({
     imageService: "cloudflare",
   }),
+
+  integrations: [preact()],
 });
