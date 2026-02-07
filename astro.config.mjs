8518d2d62129a863ad03cbd55bddb6005278b932
Diego Tenjo <tenjodiego961@gmail.com>
2026-02-06 19:39:42 -0500

    feat: Add RSS feed and configure site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-tutorial.tenjodiego961.workers.dev/",
   output: "server",
   adapter: cloudflare({
     imageService: "cloudflare",
